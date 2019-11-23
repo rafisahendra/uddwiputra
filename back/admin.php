05fdf79f8e84485d8879396c58d8af6beec340af
rafisahendra <strongdeath15@gmail.com>
2019-11-23 22:32:08 +0700

    post

--- back/admin.php
+++
@@ -1,8 +0,0 @@
-
-<form action="controller/AdmController.php?aksi=tambah" method="POST">
-    <div>
-    nama admin <br>
-    <input type="text" name="nama" placeholder="Nama Admin">
-    <button type="submit">Simpan</button>
-    </div>
-</form>
